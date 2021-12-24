04149f6f22788658fee28578bd513ecedf9ff566
luxb <luxb@dcnoahark.com>
2021-12-24 21:52:48 +0800

    添加JasperInitializer

--- java/org/apache/catalina/startup/ContextConfig.java
+++ java/org/apache/catalina/startup/ContextConfig.java
@@ -23,6 +23,7 @@ import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.util.ContextName;
 import org.apache.catalina.util.Introspection;
+import org.apache.jasper.servlet.JasperInitializer;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.apache.tomcat.JarScanType;
@@ -714,6 +715,7 @@ public class ContextConfig implements LifecycleListener {
 
         //心心念念的web.xml在这里解析的
         webConfig();
+        context.addServletContainerInitializer(new JasperInitializer(), null);
 
         if (!context.getIgnoreAnnotations()) {
             applicationAnnotationsConfig();
